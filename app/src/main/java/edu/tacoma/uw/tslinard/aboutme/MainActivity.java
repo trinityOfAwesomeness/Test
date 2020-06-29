b84953c1be02e84eda50a7665b1cba04fa20d9f5
Seoungdeok Jeon <cjhj0320@gmail.com>
2020-06-29 13:30:43 -0700

    Test Commit

--- app/src/main/java/edu/tacoma/uw/tslinard/aboutme/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/tslinard/aboutme/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /* Hello world */
+        /* Hello world */
     }
 
     public void onClick(View view) {
